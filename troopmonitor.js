b37d9c57c151c22beb5f4868e962bdafea6076a9
michael <michael@mbp.local>
2014-04-12 15:40:04 -0700

    simplified API json objects

--- troopmonitor.js
+++ troopmonitor.js
@@ -1,32 +1,69 @@
+// add clone to array prototype
+
+Array.prototype.clone = function() {
+    return this.slice(0);
+}
+
+// initialize api
 var api = pinoccioAPI();
+
+// read-only token
 api.token = "35b232f3538f0e157751f04136e2a0b4";
 api.rest({url:"https://api.pinocc.io/v1/account"}, function(err, data){
     console.log("success?", err, data);
 });
 
-
-// this example works both on server and browser with browserify.
-
-// var pinoccio = require('pinoccio');
-
-// var token = "35b232f3538f0e157751f04136e2a0b4";
-
-// To get your auth token run ```pinoccio token``` in your terminal 
-// You may need to login. To install the cli run ```sudo npm install -g pinoccio```
-
-// var api = pinoccio(token);
-
+// sync api with troop
 var s = api.sync();
 
+// data object types
+led = [];
+temp = [];
+power = [];
+
+// There are only 6 sensors
+for(var i = 0; i < 6; i++) {
+    led[i] = 0;
+    temp[i] = 0;
+    power[i] = 0;
+}
+
+// data stream
 s.on('data',function(data){
-
-  // if the event is not type data or token its something you can query from /v1/stats as report={type}
-  console.log('event!',data);
-
+    
+    var index = parseInt(data.scout) - 1;;
+    
+    switch(data.type) {
+        case "led":
+            led[index] = new LED(data);
+            break;
+        case "temp":
+            temp[index] = new Temp(data);
+            break;
+        case "power":
+            power[index] = new Power(data);
+            break;
+        default:
+            // skip data object
+    }
 }).on('error',function(error){
-
-  // after an error a new sync stream will have to be created.
-  // errors will happen due to connectivity etc.
-  console.log('sync error',error);
-
+    console.log('sync error',error);
 });
+
+// parse uselss information out of objects
+function LED(scout) {
+    this.currentColor = scout.value.led.clone();
+    this.idColor = scout.value.torch.clone();
+}
+
+function Temp(scout) {
+    this.currentTemp = scout.value.current;
+    this.localHigh = scout.value.high;
+    this.localLow = scout.value.low;
+}
+
+function Power(scout) {
+    this.batteryRemaining = scout.value.battery;
+    this.charging = scout.value.charging;
+    this.currentVoltage = scout.value.voltage;
+}
