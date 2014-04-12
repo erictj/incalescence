5a14bb1ac0b7cf2460d3c96d74ee748572ae5506
michael <michael@mbp.local>
2014-04-12 14:16:16 -0700

    js working

--- troopmonitor.js
+++ troopmonitor.js
@@ -1,13 +1,20 @@
+var api = pinoccioAPI();
+api.token = "35b232f3538f0e157751f04136e2a0b4";
+api.rest({url:"https://api.pinocc.io/v1/account"}, function(err, data){
+    console.log("success?", err, data);
+});
+
+
 // this example works both on server and browser with browserify.
 
-var pinoccio = require('pinoccio');
+// var pinoccio = require('pinoccio');
 
-var token = "my auth token here";
+// var token = "35b232f3538f0e157751f04136e2a0b4";
 
 // To get your auth token run ```pinoccio token``` in your terminal 
 // You may need to login. To install the cli run ```sudo npm install -g pinoccio```
 
-var api = pinoccio(token);
+// var api = pinoccio(token);
 
 var s = api.sync();
 
